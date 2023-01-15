b00ac25d17361bec4716dcd0827b0609336d4b0b
tripathick <chandankumar765414@gmail.com>
2023-01-15 22:44:43 +0530

    JavaScript Mini-Project

---
+++ scrool_annimation/script.js
@@ -0,0 +1,23 @@
+const boxes = document.querySelectorAll('.box');
+
+console.log(window.innerHeight);
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes();
+
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 5*4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
+        }
+        else{
+            box.classList.remove('show')
+        }
+
+    })
+}
